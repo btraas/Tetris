1b7df427b6157235fc94cd40aeff8fad01dc74f4
Brayden <btraas@gmail.com>
2017-04-05 11:40:01 -0700

    minor style changes

--- Block.js
+++ Block.js
@@ -263,9 +263,9 @@ function Shape(grid, size, color) {
             let block = this.blocks[i];
             let inner = document.createElement("div");
             inner.style.position = "absolute";
-            inner.style.left = parseInt(block.ShapePos.x+(3*block.Point.x)) + "px";
-            inner.style.top  = parseInt(block.ShapePos.y+(3*block.Point.y)) + "px";
-            inner.style.border = "1px solid #444444";
+            inner.style.left = parseInt(block.ShapePos.x)+parseInt(block.Point.x) + "px";
+            inner.style.top  = parseInt(block.ShapePos.y)+parseInt(block.Point.y) + "px";
+            inner.style.border = "0px solid #444444";
             inner.appendChild(block.element());
             div.appendChild(inner);
         }
